d157d92b1992f19ca7d9c48fc21fc245d0299712
Nella-dev <jkryu5716@gmail.com>
2026-02-26 16:25:20 +0900

    Add files via upload

---
+++ firebase_key.js
@@ -0,0 +1,10 @@
+// firebase_key.js - 이 파일은 절대 Github에 업로드하지 마세요!
+const firebaseConfig = {
+  apiKey: "AIzaSyCbqEcGsdSDBZs8PjiI05YRNEGupLf3nSc",
+  authDomain: "roider-guild-management.firebaseapp.com",
+  projectId: "roider-guild-management",
+  storageBucket: "roider-guild-management.firebasestorage.app",
+  messagingSenderId: "1012249034459",
+  appId: "1:1012249034459:web:ec0f821f29170446af96fe",
+  measurementId: "G-J7W4LFLHPP"
+};
